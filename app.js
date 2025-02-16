ea7dccfb2d5d1e79729c86683cf64b719537c85e
Dennis <dennisadev@gmail.com>
2025-02-16 12:29:53 -0500

    Exercise 1-5

--- app.js
+++ app.js
@@ -4,7 +4,50 @@ console.log(
 );
 
 // Exercise 1
+const firstName; 
+let lastName;
+var age;
 
 // Exercise 2
+firstName = "Dennis";
+lastName = "Armstrong";
+age = 39;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitiv = true;
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+//Print variables to console log
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(language);
+console.log(createdYear);
+console.log(isCaseSensitiv);
+console.log(price);
+console.log(isOnSale);
+console.log(salePercentage);
+console.log(stock);
+console.log(inStock);
+console.log(selectedSize);
+console.log(title);
+console.log(author);
+console.log(pageCount);
+console.log(bookmark);
+console.log(hasRead);
